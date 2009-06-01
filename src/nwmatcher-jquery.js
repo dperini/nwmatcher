e7a6fc0e5be92ae5dde7baa2b00c03bfca4316cf
Diego Perini <diego.perini@gmail.com>
2009-06-02 00:38:55 +0200

    changed the add-on to use the push() method, removed a compiler variable

--- src/nwmatcher-jquery.js
+++ src/nwmatcher-jquery.js
@@ -20,7 +20,7 @@ var jquery_ChildSelectors = function(match, source) {
 
   var status = true,
   // do not change this, it is searched & replaced
-  ACCEPT_NODE = 'r[X++]=N;continue main;';
+  ACCEPT_NODE = 'r.push(N);continue main;';
 
   if (match[2]) {
     match[2] = match[2].replace(/^\((.*)\)$/, '$1');
@@ -70,7 +70,7 @@ var jquery_PseudoSelectors = function(match, source) {
 
   var status = true,
   // do not change this, it is searched & replaced
-  ACCEPT_NODE = 'r[X++]=N;continue main;';
+  ACCEPT_NODE = 'r.push(N);continue main;';
 
   switch(match[1]) {
     case 'has':
