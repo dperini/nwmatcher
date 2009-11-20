589685ba35d8fcef04a0f48e8db74e7a880addd2
Diego Perini <diego@ns.iport.int>
2009-11-20 18:03:48 +0100

    forced check for ':enabled' and ':disabled' pseudo resolvers to be case insensitive (jdalton)

--- src/nwmatcher.js
+++ src/nwmatcher.js
@@ -835,11 +835,11 @@ NW.Dom = (function(global) {
               break;
             case 'enabled':
               // does not consider hidden input fields
-              source = 'if(((e.type&&e.type!=="hidden")||s.isLink(e))&&!e.disabled){' + source + '}';
+              source = 'if(((e.type&&e.type.toLowerCase()!=="hidden")||s.isLink(e))&&!e.disabled){' + source + '}';
               break;
             case 'disabled':
               // does not consider hidden input fields
-              source = 'if(((e.type&&e.type!=="hidden")||s.isLink(e))&&e.disabled){' + source + '}';
+              source = 'if(((e.type&&e.type.toLowerCase()!=="hidden")||s.isLink(e))&&e.disabled){' + source + '}';
               break;
 
             // CSS3 target pseudo-class
