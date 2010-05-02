d96865b04763e5e2da44eee1189db4731a003cbc
Diego Perini <diego@ns.iport.int>
2010-05-02 02:03:59 +0200

    update the css3-compat test, added version numbers to each framework/library, forced test to use the fall back engine instead of the native 'querySelectorAll()', fixed a problem with double firing events on Opera, relaxed comparison of 'nwmatcher' to properly configure any version >= 1.2.2, removed unneeded comment lines, bumped version

--- test/css3-compat/css3-compat.js
+++ test/css3-compat/css3-compat.js
@@ -11,25 +11,31 @@ var CDN = 'http://ajax.googleapis.com/ajax/libs/',
 //  * CDN hosted frameworks
 //  * other selector engines
 engines = {
-  'querySelectorAll': [ 'document.querySelectorAll(s)',    '' ],
-  'nwmatcher':        [ 'NW.Dom.select(s)',                '../../src/nwmatcher.js' ],
-//  'base2':            [ 'base2.dom.querySelectorAll(c,s)', 'lib/base2+dom.js' ],
-//  'dojo':             [ 'dojo.query(s)',                   CDN + 'dojo/1.4.1/dojo/dojo.xd.js' ],
-//  'ext':              [ 'Ext.DomQuery.select(s)',          CDN + 'ext-core/3.1.0/ext-core.js' ],
-//  'jquery':           [ '$(s)',                            CDN + 'jquery/1.4.2/jquery.min.js' ],
-//  'mootools':         [ '$$(s)',                           CDN + 'mootools/1.2.4/mootools.js' ],
-//  'prototype':        [ '$$(s)',                           CDN + 'prototype/1.6.1.0/prototype.js' ],
-//  'mylib':            [ 'API.getEBCS(s)',                  'lib/mylib-qsa-min.js' ],
-//  'yass':             [ '_(s)',                            'lib/yass.0.3.9.js' ],
-//  'slick':            [ 'Slick(c,s)',                      'lib/slick.js' ],
-//  'yui':              [ 'Y.Selector.query(s)',             'lib/yui3.js' ],
-//  'sly':              [ 'Sly.search(s)',                   'lib/sly.js' ],
-  'querySelectorAll': [ 'document.querySelectorAll(s)',    '' ]
+  'querySelectorAll':   [ 'document.querySelectorAll(s)',    '' ],
+
+  'nwmatcher-1.2.2':    [ 'NW.Dom.select(s)',                '../../src/nwmatcher.js' ],
+  'base2-2.0alpha1':    [ 'base2.dom.querySelectorAll(c,s)', 'lib/base2+dom.js' ],
+
+//  'prototype-1.6.1':    [ '$$(s)',                           CDN + 'prototype/1.6.1.0/prototype.js' ],
+//  'dojo-1.4.1':         [ 'dojo.query(s)',                   CDN + 'dojo/1.4.1/dojo/dojo.xd.js' ],
+//  'mootools-1.2.4':     [ '$$(s)',                           CDN + 'mootools/1.2.4/mootools.js' ],
+//  'jquery-1.4.2':       [ '$(s)',                            CDN + 'jquery/1.4.2/jquery.min.js' ],
+//  'ext-3.1.0':          [ 'Ext.DomQuery.select(s)',          CDN + 'ext-core/3.1.0/ext-core.js' ],
+
+//  'mylib-1.0':          [ 'API.getEBCS(s)',                  'lib/mylib-qsa-min.js' ],
+//  'domassistant-2.8.0': [ '$(s)',                            'lib/domass.js' ],
+//  'slick-0.9dev':       [ 'Slick.search(c,s)',               'lib/slick.js' ],
+//  'sly-1.0rc2':         [ 'Sly.search(s)',                   'lib/sly.js' ],
+//  'yui-3.0':            [ 'Y.Selector.query(s)',             'lib/yui3.js' ],
+//  'sizzle-1.0':         [ 'Sizzle(s)',                       'lib/sizzle.js' ],
+//  'yass-0.3.9':         [ '_(s)',                            'lib/yass.0.3.9.js' ],
+
+  'querySelectorAll':   [ 'document.querySelectorAll(s)',    '' ]
 };
 
 (function(global, engines) {
 
-  var version = '0.1',
+  var version = '0.2',
 
   // document reference
   doc = global.document,
@@ -141,7 +147,6 @@ engines = {
 
       // some engine will throw errors and stop processing
       // so we added a try/catch block around the method call
-      // exceptions needing different order of parameters first
       try { elements = Function('c,s', 'return ' + method)(doc, selectorText); } catch(e) { }
 
       if (elements && elements.length) {
@@ -183,12 +188,12 @@ engines = {
 
       // for nwmatcher, disable complex selectors nested in
       // :not() pseudo-classes to comply with specifications
-      if (engine == 'nwmatcher') {
+      if (engine.indexOf('nwmatcher') > -1) {
         NW.Dom.configure({ 'SIMPLENOT': true });
       }
 
       if (engine == 'querySelectorAll' && typeof doc.querySelectorAll == 'undefined') {
-        alert('This browser do not support Query Selectors API !!!\n' +
+        alert('NOTE: This browser don\'t support Query Selectors API.\n' +
           'The stylesheet will be loaded from an external css file.');
         link = doc.createElement('link');
         link.href = 'css3-compat.css';
@@ -216,18 +221,29 @@ engines = {
       select = addSelectBox();
 
       if (engine && engines[engine][1]) {
+        // overwrite native methods to force
+        // frameworks to use the fall-back
+        // branch of the selector engine
+        doc.querySelectorAll = undefined;
+        doc.querySelector = undefined;
+
         // inject a script loading the selected framework/library
         script = head.insertBefore(doc.createElement('script'), head.firstChild);
         script.type = 'text/javascript';
         script.src = engines[engine][1];
         // setup script load handlers
         script.onload = startTest;
-        script.onreadystatechange =
-          function() {
-            if (/loaded/.test(script.readyState)) {
-              startTest();
-            }
-          };
+        // need a small IE inference :(
+        // to avoid problems with Opera
+        // doesn't this make sense :?)
+        if (doc.createEventObject) {
+          script.onreadystatechange =
+            function() {
+              if (/loaded/.test(script.readyState)) {
+                startTest();
+              }
+            };
+        }
       } else {
         // no need to load a framework
         engine = 'querySelectorAll';
@@ -237,4 +253,3 @@ engines = {
     };
 
 })(this, engines);
-
