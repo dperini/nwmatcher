38913a37f4e2ace2275c4e7d62042fb46de16a88
Diego Perini <diego.perini@gmail.com>
2011-05-09 04:07:30 +0200

    cleanup and indentation, enabled errors for unresolvable contexts

--- src/nwmatcher-shortcuts.js
+++ src/nwmatcher-shortcuts.js
@@ -1,32 +1,32 @@
-NW.Dom.shortcuts = function(selector, from, alt) {
-
-  var doc = from.ownerDocument || from,
-
-  root = doc.documentElement,
+NW.Dom.shortcuts = (function() {
 
   // match missing R/L context
-  reLeftContext = /^[\x20\t\n\r\f]*[>+~]/,
+  var reLeftContext = /^[\x20\t\n\r\f]*[>+~]/,
+
   reRightContext = /[>+~][\x20\t\n\r\f]*$/;
 
-  // add left context if missing
-  if (reLeftContext.test(selector)) {
-    if (from.nodeType == 1 && from.id) {
-      selector = '#' + from.id + ' ' + selector;
-    } else if (from == root || from == doc.body) {
-      selector = from.nodeName + ' ' + selector;
-    } else if (alt) {
-      selector = NW.Dom.shortcuts(selector, alt);
-    } else {
-      selector = '#' + (from.id = 'test') + selector;
-//      NW.Dom.emit('Unable to resolve a context for the shortcut selector "' + selector + '"');
+  return function(selector, from, alt) {
+
+    var doc = from.ownerDocument || from;
+
+    // add left context if missing
+    if (reLeftContext.test(selector)) {
+      if (from.nodeType == 1 && from.id) {
+        selector = '#' + from.id + ' ' + selector;
+      } else if (from == doc.documentElement || from == doc.body) {
+        selector = from.nodeName + ' ' + selector;
+      } else if (alt) {
+        selector = NW.Dom.shortcuts(selector, alt);
+      } else {
+        NW.Dom.emit('Unable to resolve a context for the shortcut selector "' + selector + '"');
+      }
     }
-  }
 
-  // add right context if missing
-  if (reRightContext.test(selector)) {
-    selector += ' *';
-  }
+    // add right context if missing
+    if (reRightContext.test(selector)) {
+      selector += ' *';
+    }
 
-  return selector;
-
-};
+    return selector;
+  };
+})();
