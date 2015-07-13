fb8288b8aab9c7191c40475069bd0fdf3c6f99b0
Diego Perini <diego.perini@gmail.com>
2015-07-13 17:13:52 +0200

    bumped version to 1.3.6, no code changes

--- src/nwmatcher.js
+++ src/nwmatcher.js
@@ -5,7 +5,7 @@
  * nwmatcher.js - A fast CSS selector engine and matcher
  *
  * Author: Diego Perini <diego.perini at gmail com>
- * Version: 1.3.5
+ * Version: 1.3.6
  * Created: 20070722
  * Release: 20150710
  *
@@ -51,7 +51,7 @@
 
 })(this, function(global, exports) {
 
-  var version = 'nwmatcher-1.3.5',
+  var version = 'nwmatcher-1.3.6',
 
   Dom = exports,
 
