76aa23932484c59c43d686a790f08f9e77cf0ef5
Diego Perini <diego.perini@gmail.com>
2016-11-26 15:24:14 +0100

    moved to beta version, preparing for next release

--- src/nwmatcher-noqsa.js
+++ src/nwmatcher-noqsa.js
@@ -5,9 +5,9 @@
  * nwmatcher-noqsa.js - A fast CSS selector engine and matcher
  *
  * Author: Diego Perini <diego.perini at gmail com>
- * Version: 1.3.9
+ * Version: 1.4.0beta
  * Created: 20070722
- * Release: 20161026
+ * Release: 20161126
  *
  * License:
  *  http://javascript.nwbox.com/NWMatcher/MIT-LICENSE
@@ -28,7 +28,7 @@
 
 })(this, function(global) {
 
-  var version = 'nwmatcher-1.3.9',
+  var version = 'nwmatcher-1.4.0beta',
 
   doc = global.document,
   root = doc.documentElement,
